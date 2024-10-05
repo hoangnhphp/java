9eb610029464e2a622baa7db6016a69157364603
hoangNH <hoangcdnbk@gmail.com>
2024-10-05 15:01:48 +0700

    add java

---
+++ min-in-array/src/MinInArray.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class MinInArray {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size:");
+            size = scanner.nextInt();
+            if (size > 20)
+                System.out.println("Size does not exceed 20");
+        } while (size > 20);
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+        System.out.printf("Elements in array: ", "");
+        for (int value : array) {
+            System.out.print(value + "\t");
+        }
+        int min = array[0];
+        for (int j = 1; j < array.length; j++) {
+            if (array[j] < min) {
+                min = array[j];
+            }
+        }
+        System.out.println("\n");
+        System.out.println("Min number in array: " + min);
+    }
+}
