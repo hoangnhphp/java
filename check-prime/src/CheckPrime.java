bce90a83f2b53726aa93d1f39e9053a6206f167d
hoangNH <hoangcdnbk@gmail.com>
2024-10-07 18:52:36 +0700

    add java

--- check-prime/src/CheckPrime.java
+++ check-prime/src/CheckPrime.java
@@ -2,10 +2,11 @@ import java.util.Scanner;
 
 public class CheckPrime {
     public static void main(String[] args) {
-        int max = 100;
+        int max = 100, i;
+        boolean check;
         for (int number = 2; number <= max; number++) {
-            boolean check = true;
-            int i = 2;
+            check = true;
+            i = 2;
             while (i <= Math.sqrt(number)) {
                 if (number % i == 0) {
                     check = false;
