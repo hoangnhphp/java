af958c23e7d552e229d35a3c02bcfbe7a17d36c5
hoangNH <hoangcdnbk@gmail.com>
2024-10-09 19:35:02 +0700

    add java

---
+++ access-modifier/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Student st = new Student();
+        st.setName("Hoang");
+        st.setClassName("Java");
+        System.out.println("Student's name is " + st.getName() + " and class name is " + st.getClassName());
+    }
+}
---
+++ access-modifier/src/Student.java
@@ -0,0 +1,22 @@
+public class Student {
+    private String name = "John";
+    private String className = "CO2";
+
+    public Student() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+}
