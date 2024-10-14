f50f3258f1bdede3b3398702ff3098773278a6b7
hoangNH <hoangcdnbk@gmail.com>
2024-10-14 09:06:30 +0700

    add java

---
+++ point-2d-and-3d/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Point2D point2d = new Point2D();
+        point2d.setX(10);
+        point2d.setY(10);
+
+        System.out.println(point2d);
+
+        Point3D point3d = new Point3D();
+        point3d.setX(10);
+        point3d.setY(10);
+        point3d.setZ(10);
+
+        System.out.println(point3d);
+    }
+}
