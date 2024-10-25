4177c2aba925def11dec71d56c9bf727899e473c
hoangNH <hoangcdnbk@gmail.com>
2024-10-25 19:57:04 +0700

    add java

--- demerger/src/Demerger.java
+++ demerger/src/Demerger.java
@@ -1,31 +1,30 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Demerger {
     public static void main(String[] args) {
+
         Queue<Employee> NU = new LinkedList<>();
         Queue<Employee> NAM = new LinkedList<>();
 
         // Giả sử dữ liệu đã được đọc từ file và đưa vào danh sách records
-        Employee[] records = {
-                new Employee("Anna", "Female", "1990-01-01"),
-                new Employee("John", "Male", "1985-05-20"),
-                new Employee("Lisa", "Female", "1992-03-15"),
-                new Employee("Mark", "Male", "1988-07-30")
-        };
-
+        List<Employee> employees = new ArrayList<>(
+                Arrays.asList(new Employee("Anna", "Female", "1999-01-03"),
+                        new Employee("John", "Male", "1985-05-20"),
+                        new Employee("Lisa", "Female", "1992-03-15"),
+                        new Employee("Mark", "Male", "1988-07-30"))
+        );
+        Collections.sort(employees, Comparator.comparing(emp -> emp.getBirthdate()));
         // Phân loại các bản ghi vào các Queue
-        for (Employee record : records) {
+        for (Employee record : employees) {
             if (record.getGender().equalsIgnoreCase("Female")) {
                 NU.add(record);
             } else {
                 NAM.add(record);
             }
         }
-
         // Ghi dữ liệu vào file output
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("sorted_records.txt"))) {
             while (!NU.isEmpty()) {
