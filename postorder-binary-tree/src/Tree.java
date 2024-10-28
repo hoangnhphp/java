14618a35121d21aa72664778cdd8b97e119c4965
hoangNH <hoangcdnbk@gmail.com>
2024-10-28 18:50:15 +0700

    add java

---
+++ postorder-binary-tree/src/Tree.java
@@ -0,0 +1,20 @@
+public interface Tree<E> {
+    /**
+     * Insert element e into the binary search tree.
+     * Return true if the element is inserted successfully.
+     */
+    public boolean insert(E e);
+    /**
+     * Inorder traversal from the root
+     */
+    public void inorder();
+
+    /**
+     * Post-order traversal from the root
+     */
+    public void postorder();
+    /**
+     * Get the number of nodes in the tree
+     */
+    public int getSize();
+}
