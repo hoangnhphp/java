159cdc529c2eb61cca30371e00e050cdab2434ca
hoangNH <hoangcdnbk@gmail.com>
2024-10-28 19:06:27 +0700

    add java

---
+++ preorder-binary-tree/src/AbstractTree.java
@@ -0,0 +1,8 @@
+public abstract class AbstractTree<E> implements Tree<E> {
+    @Override /** Inorder traversal from the root*/
+    public void inorder() {
+    }
+
+    public void postorder() {}
+    public void preorder() {}
+}
---
+++ preorder-binary-tree/src/TestBST.java
@@ -0,0 +1,17 @@
+public class TestBST {
+    public static void main(String[] args) {
+        //create a BST
+        BST<Integer> tree = new BST<>();
+        tree.insert(20);
+        tree.insert(15);
+        tree.insert(26);
+        tree.insert(14);
+        tree.insert(18);
+        tree.insert(24);
+        tree.insert(28);
+        //traverse tree
+        System.out.println("Post-order (sorted): ");
+        tree.preorder();
+        System.out.println("The number of nodes is: " + tree.getSize());
+    }
+}
